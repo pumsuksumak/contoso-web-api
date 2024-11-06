49da3bd6105deac7c82e77781c77062222e7ca81
Sureeporn Suk <sureepos@ais.co.th>
2024-11-06 08:57:46 +0000

    Version2.0.1!

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello World! - 2.0.0');
+    res.send('Hello World! - 2.0.0.1');
 });
 app.listen(PORT, HOST, () =>{
     console.log(`Running On http://${HOST}:${PORT}`);
